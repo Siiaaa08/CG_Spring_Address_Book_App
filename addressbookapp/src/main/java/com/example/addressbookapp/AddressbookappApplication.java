8cb71a4e3127782465f833dcbe0e49c05518b8a3
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-05 13:49:08 -0800

    [Saloni]Add Code of logger which gives an information to logger

--- addressbookapp/src/main/java/com/example/addressbookapp/AddressbookappApplication.java
+++ addressbookapp/src/main/java/com/example/addressbookapp/AddressbookappApplication.java
@@ -1,14 +1,16 @@
 package com.example.addressbookapp;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressbookappApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(AddressbookappApplication.class);
 	public static void main(String[] args) {
 
 		SpringApplication.run(AddressbookappApplication.class, args);
+		log.info("App has started!!!");
 	}
 
 }
